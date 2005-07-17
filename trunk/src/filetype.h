d61fa711d8ad373164dd12b6dea9a2d071909c64
nelchael <nelchael@b86d363e-fefa-0310-9b2f-cf97e715fe77>
2005-07-17 13:31:08 +0000

    Usuniecie autom4te.cache

--- trunk/src/filetype.h
+++ trunk/src/filetype.h
@@ -2,15 +2,15 @@
 #define filetype
 
 //icon and colors associated with filetype
-class file_type
-{
-public:
-FXIcon *icon;
-FXColor color;
-FXColor backcolor;
+class file_type {
+	
+	public:
+		FXIcon *icon;
+		FXColor color;
+		FXColor backcolor;
 
-file_type::file_type(FXIcon*ic,FXColor col,FXColor backcol);
-file_type::file_type(){}
+		file_type::file_type(FXIcon*ic,FXColor col,FXColor backcol);
+		file_type::file_type(){}
 
 };
 
