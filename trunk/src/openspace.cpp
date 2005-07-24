95c00a55bd3cf9e5cd74252996b89286b8abcbe6
compbatant <compbatant@b86d363e-fefa-0310-9b2f-cf97e715fe77>
2005-07-24 22:22:48 +0000

    upiekszanie kodu ;p

--- trunk/src/openspace.cpp
+++ trunk/src/openspace.cpp
@@ -1,21 +1,21 @@
 #include "fx.h"
 #include "mainwindow.h"
 #include <unistd.h>
- 
+ 
 //-----MAIN------------------------------------------------------------------------------------------------------------------------------------------ 
 //nothing special here, create and execute main window
     int
 main (int argc, char **argv) 
 {
-    fxTraceLevel = 10;
-    FXApp * application = new FXApp ("openspace", NULL);
-    application->init (argc, argv);
-    fxmessage (argv[0]);
-    new MainWindow (application);
-    application->create ();
-    return application->run ();
-}
+    fxTraceLevel = 10;
+    FXApp * application = new FXApp ("openspace", NULL);
+    application->init (argc, argv);
+    fxmessage (argv[0]);
+    new MainWindow (application);
+    application->create ();
+    return application->run ();
+}
 
-
+
 //-----MAIN------------------------------------------------------------------------------------------------------------------------------------------       
     
