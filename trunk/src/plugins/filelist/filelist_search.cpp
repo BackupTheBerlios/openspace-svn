f397bfb66222b17d615a785ddeca8f571cabd2f7
compbatant <compbatant@b86d363e-fefa-0310-9b2f-cf97e715fe77>
2005-08-06 19:57:08 +0000

    plugin do wyszukiwania plikow

---
+++ trunk/src/plugins/filelist/filelist_search.cpp
@@ -0,0 +1,130 @@
+#ifdef WIN32
+#define EXPORTFUNCTION extern "C" __declspec(dllexport)
+#else
+#define EXPORTFUNCTION extern "C"
+#endif
+#include "filelist_search.h"
+int filelist_search::osopendir (string dir)
+{
+
+    
+
+}
+osfile filelist_search::osreaddir (void)
+{
+    osfile os_file;
+
+    if(count!=filesnum)
+    {
+	    os_file.type = 0;
+	    os_file.name = files[count];
+	    os_file.size = 300;
+	    os_file.attrib = new string[2];
+	    os_file.attrib[0] = "300";
+	    os_file.attrib[1] = "";
+	    
+	    count++;
+	    
+	    return os_file;
+
+    }
+    
+    os_file.name = "";
+    count=0;
+    return os_file;
+    
+
+
+}
+int filelist_search::mkdir (string dir, int mode)
+{
+}
+int filelist_search::copy (thread_elem * te)
+{
+fxmessage("COPY\n"); 
+
+}
+int filelist_search::move (thread_elem * te)
+{
+}
+int filelist_search::remove (thread_elem * te)
+{
+}
+int filelist_search::rename (string orgname, string newname)
+{
+
+}
+int filelist_search::init (vector < string > *vector_name, pathtype pt, configure * conf)
+{
+fxmessage("INIT");
+filesnum=0;
+count=0;
+    pipe = popen (pt.server.c_str(), "r");
+ fxmessage(pt.server.c_str());  
+    if (pipe != NULL)
+    {
+	string line;
+	while (fgets (readbuf, sizeof (readbuf), pipe) != NULL)
+	{
+
+
+	    string path = readbuf;
+	    path = path.substr (0, path.length () - 1);
+	    files.push_back(path);
+	    filesnum++;
+	}
+
+	pclose (pipe);
+    }
+    
+
+    
+
+}
+int filelist_search::mode (string file)
+{
+}
+string filelist_search::owner (string file)
+{
+return "";
+}
+string filelist_search::group (string file)
+{
+return "";
+}
+bool filelist_search::mode (string file, unsigned int, bool recursive)
+{
+}
+bool filelist_search::owner (string file, string, bool recursive)
+{
+}
+bool filelist_search::group (string file, string, bool recursive)
+{
+}
+string filelist_search::info (void)
+{
+return "";
+}
+void filelist_search::totalsize (string path, unsigned long &size)
+{
+}
+string filelist_search::symlink (string path)
+{
+}
+bool filelist_search::symlink (string src, string dst)
+{
+}
+bool filelist_search::hardlink (string src, string dst)
+{
+}
+int filelist_search::supportedfunctions (void)
+{
+}
+int filelist_search::quit (void)
+{
+}
+EXPORTFUNCTION filelist_base *get_filelist (void)
+{
+    FXTRACE ((5, "PLUGIN SEARCH LOAD\n"));
+    return new filelist_search ();
+}
---
+++ trunk/src/plugins/filelist/filelist_search.h
@@ -0,0 +1,38 @@
+#ifndef FILELIST_search
+#define FILELIST_search
+#include "../../filelist_base.h"
+class filelist_search:public filelist_base
+{
+  public:
+  
+  
+  
+  FILE *pipe;
+  char readbuf[1024];
+  vector < string > files;
+  int count;
+  int filesnum;
+  
+    int filelist_search::osopendir (string dir);
+    osfile filelist_search::osreaddir (void);
+    int filelist_search::mkdir (string dir, int mode = 0);
+    int filelist_search::copy (thread_elem * te);
+    int filelist_search::move (thread_elem * te);
+    int filelist_search::remove (thread_elem * te);
+    int filelist_search::rename (string orgname, string newname);
+    int filelist_search::init (vector < string > *vector_name, pathtype pt, configure * conf);
+    int filelist_search::mode (string file);
+    string filelist_search::owner (string file);
+    string filelist_search::group (string file);
+    bool filelist_search::mode (string file, unsigned int, bool recursive);
+    bool filelist_search::owner (string file, string, bool recursive);
+    bool filelist_search::group (string file, string, bool recursive);
+    string filelist_search::info (void);
+    void filelist_search::totalsize (string path, unsigned long &size);
+    string filelist_search::symlink (string path);
+    bool filelist_search::symlink (string src, string dst);
+    bool filelist_search::hardlink (string src, string dst);
+    int filelist_search::supportedfunctions (void);
+    int filelist_search::quit (void);
+};
+#endif
