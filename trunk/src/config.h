bdfef4bd88b3d96d79a225bc3423efef445ad045
compbatant <compbatant@b86d363e-fefa-0310-9b2f-cf97e715fe77>
2006-05-13 11:38:08 +0000

    0.2.1 rc1

--- trunk/src/config.h
+++ trunk/src/config.h
@@ -140,19 +140,19 @@
 #define PACKAGE_NAME "nao"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "nao 0.2.0"
+#define PACKAGE_STRING "nao 0.2.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "nao"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.2.0"
+#define PACKAGE_VERSION "0.2.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.2.0"
+#define VERSION "0.2.1"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
